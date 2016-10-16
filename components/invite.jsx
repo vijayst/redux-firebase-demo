ef2aafa5116f0ee38b694c4870d3764eabaecfc4
vijayst <vijayst@gmail.com>
2016-10-16 13:01:25 +0530

    styling for meeting summary.

--- components/invite.jsx
+++ components/invite.jsx
@@ -13,10 +13,31 @@ export default class Invite extends React.Component {
         <div className="well">
           <h1>Meeting invite</h1>
         </div>
-        <div>
-          {host}
-          {agenda}
-          {guest_count}
+        <div className="bg-warning meeting-summary">
+          <div className="row">
+            <div className="col-sm-4 col-md-2">
+              <b>Host:</b>
+            </div>
+            <div className="col-sm-8 col-md-10">
+              {host}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-4 col-md-2">
+              <b>Agenda:</b>
+            </div>
+            <div className="col-sm-8 col-md-10">
+              {agenda}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-4 col-md-2">
+              <b>Guest count:</b>
+            </div>
+            <div className="col-sm-8 col-md-10">
+              {guest_count}
+            </div>
+          </div>
         </div>
       </div>
     );
