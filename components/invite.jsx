e0ee035f556da5df5770592cd2f5df2376cd194b
vijayst <vijayst@gmail.com>
2016-10-16 11:04:13 +0530

    added bootstrap-sass.

--- components/invite.jsx
+++ components/invite.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default class Invite extends React.Component {
   render() {
     return (
-      <div>
-        Hello world
+      <div className="container">
+        <div className="well">
+          <h1>Meeting invite</h1>
+        </div>
       </div>
     );
   }
